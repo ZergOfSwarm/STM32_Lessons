cd6145f16923269a64342251a14d7b70323d908b
ZergOfSwarm <d358468115905@gmail.com>
2022-04-20 17:14:55 +0300

    LWIP HTTP A

---
+++ LAN_zagotovka/Core/Inc/http_ssi.h
@@ -0,0 +1,14 @@
+/*
+ * http_ssi.h
+ *
+ *  Created on: 11-Oct-2021
+ *      Author: controllerstech
+ */
+
+#ifndef INC_HTTP_SSI_H_
+#define INC_HTTP_SSI_H_
+
+void http_server_init (void);
+
+
+#endif /* INC_HTTP_SSI_H_ */
