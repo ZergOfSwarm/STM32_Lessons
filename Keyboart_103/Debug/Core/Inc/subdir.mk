8ce8297dc9c5970fbd79b34f46bb40c445219080
denis <d358468115905@gmail.com>
2023-02-05 18:36:04 +0200

    Keyboard on Blue Pill

---
+++ Keyboart_103/Debug/Core/Inc/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Inc/keyBoard.c 
+
+OBJS += \
+./Core/Inc/keyBoard.o 
+
+C_DEPS += \
+./Core/Inc/keyBoard.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Inc/%.o Core/Inc/%.su: ../Core/Inc/%.c Core/Inc/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../USB_DEVICE/App -I../USB_DEVICE/Target -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Core-2f-Inc
+
+clean-Core-2f-Inc:
+	-$(RM) ./Core/Inc/keyBoard.d ./Core/Inc/keyBoard.o ./Core/Inc/keyBoard.su
+
+.PHONY: clean-Core-2f-Inc
+
